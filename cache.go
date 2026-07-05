49c914d4e99eeee90c79bedb4a09d12b85e535eb
prototype585 <prototype585@protonmail.com>
2026-07-05 22:19:17 +0200

    removed mutex from cache since lru cache already has mutex built in

--- cache.go
+++ cache.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"log/slog"
-	"sync"
 
 	lru "github.com/hashicorp/golang-lru/v2"
 )
@@ -14,7 +12,6 @@ type UserCache struct {
 }
 
 var userCache *lru.Cache[int64, *UserCache]
-var userCacheMutex sync.RWMutex
 
 func SetupCache() (err error) {
 	userCache, err = lru.New[int64, *UserCache](4096 * 4)
@@ -22,34 +19,29 @@ func SetupCache() (err error) {
 }
 
 func UserCacheGetSet(db *sql.DB, userId int64) (*UserCache, error) {
-	slog.Debug("Getting from cache")
-	userCacheMutex.RLock()
 	data, exists := userCache.Get(userId)
-	userCacheMutex.RUnlock()
-
 	if exists {
 		return data, nil
-	} else {
-		err := UserCacheRefresh(db, userId)
-		if err != nil {
-			return nil, err
-		}
-		return UserCacheGetSet(db, userId)
 	}
+
+	err := UserCacheRefresh(db, userId)
+	if err != nil {
+		return nil, err
+	}
+
+	return UserCacheGetSet(db, userId)
 }
 
 func UserCacheRefresh(db *sql.DB, userId int64) error {
-	slog.Debug("Getting from db")
-	var u UserCache
+	var data UserCache
+
 	row := db.QueryRow("SELECT display_name, picture FROM users WHERE id = ?", userId)
-	err := row.Scan(&u.displayName, &u.picture)
+	err := row.Scan(&data.displayName, &data.picture)
 	if err != nil {
 		return err
 	}
 
-	userCacheMutex.Lock()
-	userCache.Add(userId, &u)
-	userCacheMutex.Unlock()
+	userCache.Add(userId, &data)
 
 	return nil
 }
